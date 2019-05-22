9e0d38a988cde6671350be0adaff1312f4a88a1f
Martin Zurita <martinzurita1@gmail.com>
2019-05-22 10:15:42 -0300

    Add files via upload

---
+++ CheckActivities.ps1
@@ -0,0 +1,36 @@
+﻿#Datos y login de Service Principal
+$secpasswd = ConvertTo-SecureString "SecretKey" -AsPlainText -Force
+$creds = New-Object System.Management.Automation.PSCredential ("AppID", $secpasswd)
+Login-AzureRmAccount -Credential $creds -ServicePrincipal -TenantId "TenantID"
+Select-AzureRmSubscription -SubscriptionName Investigacion
+
+#Datos de Data Factory
+$ADFName = "DataFactoryName"
+$pipeName = "PipelineName"
+$RGName = "ResourceGroupName"
+
+$runID = "pipeRunId"
+
+
+$ahora = Get-Date
+$antes = (Get-Date).AddDays(-1)
+
+$activities = Get-AzureRmDataFactoryV2ActivityRun -DataFactoryName $ADFName -PipelineRunId $runID -ResourceGroupName $RGName `
+                                    -RunStartedAfter $antes -RunStartedBefore $ahora
+
+foreach($activ in $actividades){
+    if($activ.ActivityName -like "activities"){
+        $activ
+        $tiempoActiv = $activ.ActivityRunEnd - $activ.ActivityRunStart
+        Write-Output "Duracion de actividad: $($tiempoActiv)"
+    }
+}
+
+$i = 0
+foreach($activ in $activities){
+    if($activ.Status -like "Failed"){
+        $activ.ActivityName
+        $i++
+    }
+}
+$i
