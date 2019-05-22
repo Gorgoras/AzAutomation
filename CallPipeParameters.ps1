ee463e8701bfb41a79b970149173c6aede414fe0
Martin Zurita <martinzurita1@gmail.com>
2019-05-22 16:14:01 -0300

    Add files via upload

---
+++ CallPipeParameters.ps1
@@ -0,0 +1,12 @@
+﻿Login-AzureRmAccount
+Select-AzureRmSubscription -Subscription "yourSubId"
+
+$dfname = "youDataFActoryName"
+$rgName = "yourResourceGroupName"
+$pipe = "pipeName"
+$parameters = @{
+    "param1" = "asdasd"
+    "param2" = "123456"
+}
+
+Invoke-AzureRmDataFactoryV2Pipeline -DataFactoryName $dfname -ResourceGroupName $rgName -PipelineName $pipe -Parameter $parameters
